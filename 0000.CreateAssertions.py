672167dbb87bb7ccc140728f01de08a27bc92d0b
Kishan Thakurani <kishanthakurani2003@gmail.com>
2025-01-05 22:48:59 +0530

    2381. Shifting Letters II

--- 0000.CreateAssertions.py
+++ 0000.CreateAssertions.py
@@ -62,27 +62,22 @@ def testcases(case: str, num: int):
         print(f'Solution().{name}({collected})')
 
 
-name = "waysToSplitArray"
+name = "shiftingLetters"
 func = ""
 func2 = ""
 formatLC("""
-Example 1:
+xample 1:
 
-Input: nums = [10,4,-8,7]
-Output: 2
-Explanation: 
-There are three ways of splitting nums into two non-empty parts:
-- Split nums at index 0. Then, the first part is [10], and its sum is 10. The second part is [4,-8,7], and its sum is 3. Since 10 >= 3, i = 0 is a valid split.
-- Split nums at index 1. Then, the first part is [10,4], and its sum is 14. The second part is [-8,7], and its sum is -1. Since 14 >= -1, i = 1 is a valid split.
-- Split nums at index 2. Then, the first part is [10,4,-8], and its sum is 6. The second part is [7], and its sum is 7. Since 6 < 7, i = 2 is not a valid split.
-Thus, the number of valid splits in nums is 2.
+Input: s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]
+Output: "ace"
+Explanation: Firstly, shift the characters from index 0 to index 1 backward. Now s = "zac".
+Secondly, shift the characters from index 1 to index 2 forward. Now s = "zbd".
+Finally, shift the characters from index 0 to index 2 forward. Now s = "ace".
 Example 2:
 
-Input: nums = [2,3,1,0]
-Output: 2
-Explanation: 
-There are two valid splits in nums:
-- Split nums at index 1. Then, the first part is [2,3], and its sum is 5. The second part is [1,0], and its sum is 1. Since 5 >= 1, i = 1 is a valid split. 
-- Split nums at index 2. Then, the first part is [2,3,1], and its sum is 6. The second part is [0], and its sum is 0. Since 6 >= 0, i = 2 is a valid split.
- """)
+Input: s = "dztz", shifts = [[0,0,0],[1,1,1]]
+Output: "catz"
+Explanation: Firstly, shift the characters from index 0 to index 0 backward. Now s = "cztz".
+Finally, shift the characters from index 1 to index 1 forward. Now s = "catz".
+  """)
 # testcases(""" """", 0)
