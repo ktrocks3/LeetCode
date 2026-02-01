ceb3b5a3d1ada1d403f6584ffa4d78c7dbb1c42a
Kishan Thakurani <kishanthakurani2003@gmail.com>
2026-02-01 18:58:19 +0100

    3010. Divide an Array Into Subarrays With Minimum Cost I

--- 0000.CreateAssertions.py
+++ 0000.CreateAssertions.py
@@ -62,23 +62,33 @@ def testcases(case: str, num: int):
         print(f'Solution().{name}({collected})')
 
 
-name = "dailyTemperatures"
+name = "minimumCost"
 func = ""
 func2 = ""
 formatLC("""
 
+
 Example 1:
 
-Input: temperatures = [73,74,75,71,69,72,76,73]
-Output: [1,1,4,2,1,1,0,0]
+Input: nums = [1,2,3,12]
+Output: 6
+Explanation: The best possible way to form 3 subarrays is: [1], [2], and [3,12] at a total cost of 1 + 2 + 3 = 6.
+The other possible ways to form 3 subarrays are:
+- [1], [2,3], and [12] at a total cost of 1 + 2 + 12 = 15.
+- [1,2], [3], and [12] at a total cost of 1 + 3 + 12 = 16.
 Example 2:
 
-Input: temperatures = [30,40,50,60]
-Output: [1,1,1,0]
+Input: nums = [5,4,3]
+Output: 12
+Explanation: The best possible way to form 3 subarrays is: [5], [4], and [3] at a total cost of 5 + 4 + 3 = 12.
+It can be shown that 12 is the minimum cost achievable.
 Example 3:
 
-Input: temperatures = [30,60,90]
-Output: [1,1,0]
+Input: nums = [10,3,1,1]
+Output: 12
+Explanation: The best possible way to form 3 subarrays is: [10,3], [1], and [1] at a total cost of 10 + 1 + 1 = 12.
+It can be shown that 12 is the minimum cost achievable.
+ 
  
  """)
 # testcases(""" """, 1)
